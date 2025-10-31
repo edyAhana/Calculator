834a953b102f6f7d637e4696685150b5d3026f06
Fedor Suhanov <fedorsuhanov@gmail.com>
2025-10-31 22:45:14 +0300

    delete some methods

--- include/Parser.h
+++ include/Parser.h
@@ -1,19 +1,34 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#include "BinaryTree.h"
 #include <vector>
+#include <string>
+#include <memory>
+
+#include "AbstractSyntaxTree.h"
+
+struct Token {
+    enum class Type {
+        Function,
+        BinFunction,
+        UnFunction,
+        Number,
+        LeftBracket,
+        RightBracket,
+    };
+    
+    Type type;
+    std::string token;
+};
 
 class Parser {
 private:
     using tokens = std::vector<Token>;
-
-    static tokens tokinizer(std::string_view expression);
-    static BinaryTree::Pointer getAbstractSyntaxTree(const tokens& tokens);
     static std::size_t getPriority(const Token& token);
-    static BinaryTree::Pointer getAbstractSyntxTreeHelper(const tokens& tokens);
 public:
-    static BinaryTree Parse(std::string_view expression); 
+    static std::unique_ptr<BasicNode> parse(const tokens& tokens);
+    static tokens tokenize(const std::string& expression);
+    static std::unique_ptr<BasicNode> Parse(const std::string& expression); 
 };
 
 #endif
