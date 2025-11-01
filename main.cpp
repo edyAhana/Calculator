315f9da148ba949fc1271f8f16b365e7c95d7cb7
Fedor Suhanov <fedorsuhanov@gmail.com>
2025-11-01 18:09:02 +0300

    add main file

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <memory>
+#include <iostream>
+
+#include "Parser.h"
+#include "PluginManager.h"
+#include "Executer.h"
+
+
+void print(BasicNode* node, std::size_t offset = 0);
+
+int main() {
+    PluginManager pm;
+    pm.LoadPlugins("./plugins");
+    bool isExit = false;
+
+    while(!isExit) {
+        std::string expression;
+        std::cout << "\nenter the expression:\n";
+        std::cout << "<<< ";
+        std::getline(std::cin, expression);
+
+        if(expression == "q") {
+            isExit = true;
+            continue;
+        }
+
+        std::cout << "\nsolution:\n";
+        std::cout << "<<< ";
+        
+        double solution = 0;
+        try {
+            solution = Executer::execute(
+                pm, Parser::parse(Parser::tokenize(expression))
+            );
+        } catch (const std::exception& e) {
+            std::cerr << e.what();
+            continue;
+        }
+
+        std::cout << solution << "\n";
+    }
+
+    return 0;
+}
+
